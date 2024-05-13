75fe8bb87c1ad3c0436c4609f62fb2213b32ef13
lapkin <lapkinu@gmail.com>
2024-05-13 23:07:46 +0300

    "Rename EntryPoint"

--- src/main/java/com/javarush/caesarcipher/lapkin/EntryPoint.java
+++ src/main/java/com/javarush/caesarcipher/lapkin/EntryPoint.java
@@ -3,7 +3,7 @@ package com.javarush.caesarcipher.lapkin;
 import com.javarush.caesarcipher.lapkin.gui.SwingMenu;
 
 import javax.swing.*;
-public class Entrypoint {
+public class EntryPoint {
     public static void main(String[] args) {
         SwingUtilities.invokeLater(SwingMenu::new);
     }
