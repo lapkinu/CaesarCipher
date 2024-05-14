7304f3d8ae6d61c1e5381d0e4bd4bfce31d5e4bd
Lapkin <lapkinu@gmail.com>
2024-05-14 16:26:13 +0300

    "added an application icon"

--- src/main/java/com/javarush/caesarcipher/lapkin/gui/SwingMenu.java
+++ src/main/java/com/javarush/caesarcipher/lapkin/gui/SwingMenu.java
@@ -14,6 +14,9 @@ public class SwingMenu extends JFrame {
         setLocationRelativeTo(null);
         setDefaultCloseOperation(EXIT_ON_CLOSE);
 
+        ImageIcon icon = new ImageIcon("src/main/java/com/javarush/caesarcipher/lapkin/resources/images/im_pic2.jpg");
+        setIconImage(icon.getImage());
+
         JPanel panel = new JPanel();
         panel.setLayout(new GridLayout(4, 1, 10, 10));
 
